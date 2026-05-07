a83af2f2b66b6616729002132ad8ef30cd08002c
Aditya Kumar Srivastava <adityasri2501@gmail.com>
2026-05-08 00:05:14 +0530

    fixe logincontroller.java

--- src/main/java/com/zerotrust/controller/LoginController.java
+++ src/main/java/com/zerotrust/controller/LoginController.java
@@ -5,33 +5,26 @@ import org.springframework.web.bind.annotation.*;
 
 import java.sql.*;
 
-@RestController
-public class LoginController {
+@PostMapping("/login")
+public String login(@RequestParam String username, @RequestParam String password) {
 
-    // Hardcoded secret (for Gitleaks)
-    // String API_KEY = 
-    // for normal
-    String API_KEY = System.getenv("API_KEY");
+    try (Connection conn = DBUtil.getConnection();
+         PreparedStatement pstmt = conn.prepareStatement(
+                 "SELECT * FROM users WHERE username=? AND password=?")) {
 
-    @PostMapping("/login")
-    public String login(@RequestParam String username, @RequestParam String password) {
+        pstmt.setString(1, username);
+        pstmt.setString(2, password);
 
-        try (Connection conn = DBUtil.getConnection();
-            PreparedStatement pstmt = conn.prepareStatement(
-            "SELECT * FROM users WHERE username=? AND password=?")) {
-
-            pstmt.setString(1, username);
-            pstmt.setString(2, password);
-
-            try (ResultSet rs = pstmt.executeQuery()) {
-                if (rs.next()) {
-                    return "Login Success";
-                } else {
-            return "Login Failed";
+        try (ResultSet rs = pstmt.executeQuery()) {
+            if (rs.next()) {
+                return "Login Success";
+            } else {
+                return "Login Failed";
             }
-        } catch (SQLException e) {
-            return "Error";
         }
+
+    } catch (Exception e) {
+        e.printStackTrace(); // debugging ke liye
+        return "Error";
     }
-}
 }
